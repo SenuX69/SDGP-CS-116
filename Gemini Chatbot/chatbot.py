59d9c65e1113bc83bd256f2d34dadf684fb47ea9
thathsara336 <thathsara.20241311@iit.ac.lk>
2026-03-16 11:15:55 +0530

    Update chatbot.py

--- Gemini Chatbot/chatbot.py
+++ Gemini Chatbot/chatbot.py
@@ -1,1 +1,10 @@
 
+#PathFinder+ Career Guidance Chatbot
+
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+from pymongo import MongoClient
+
+# Step 1: Load API keys from .env file
+load_dotenv()
