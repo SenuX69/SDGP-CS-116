925739c944d0ed2c743dda4aa790f7b1db6b6a37
thathsara336 <thathsara.20241311@iit.ac.lk>
2026-03-16 11:17:13 +0530

    Update chatbot.py by env dotenev addition

--- Gemini Chatbot/chatbot.py
+++ Gemini Chatbot/chatbot.py
@@ -8,3 +8,7 @@ from pymongo import MongoClient
 
 # Step 1: Load API keys from .env file
 load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MONGO_URI      = os.getenv("MONGO_URI")
+DATABASE_NAME  = os.getenv("DATABASE_NAME", "pathfinder_plus")
