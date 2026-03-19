5a62f9710ff0f9560c052a4e564c420a8fd7bca5
Nithila Mandiw <nithilamandiw@gmail.com>
2026-03-19 18:47:56 +0530

    backend: add supabase database connection

---
+++ backend/app/db.py
@@ -0,0 +1,6 @@
+from supabase import create_client
+
+SUPABASE_URL = "https://fivqjyegpeatgeatbbdj.supabase.co"
+SUPABASE_KEY = "sb_publishable_Bt0svPzhOpYB8X_2tWAv_g_-9bb0D89"
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
