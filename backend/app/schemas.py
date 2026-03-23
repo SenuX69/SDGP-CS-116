f3ed6b99c0512d761352d759e9e8ab49e9d9a176
Senuth Dilwan <115574985+SenuX69@users.noreply.github.com>
2026-03-23 11:45:58 +0530

    Fix conflict errors in schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -10,11 +10,9 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
-
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
-
 
 class UserOut(BaseModel):
     id: int
